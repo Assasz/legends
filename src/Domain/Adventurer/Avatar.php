6b65446315fc789bf5f8fffc869b130a5fd11a40
Assasz <contact@pawelantosiak.com>
2025-07-23 00:12:32 +0200

    [feature] Adventurer avatar

--- src/Domain/Adventurer/Avatar.php
+++ src/Domain/Adventurer/Avatar.php
@@ -6,6 +6,6 @@ namespace Legends\Game\Domain\Adventurer;
 
 enum Avatar: string
 {
-    case AVATAR_1 = 'avatar_1.png';
-    case AVATAR_2 = 'avatar_2.png';
+    case AVATAR_1 = 'avatar_1';
+    case AVATAR_2 = 'avatar_2';
 }
